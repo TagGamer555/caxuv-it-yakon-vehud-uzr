822889786279d9689c93d9533b3638f612b383a1
Alexander <alexandr.11071997@gmail.com>
2024-01-14 16:36:50 +0000

    added bulk! yay!

--- scripts/units.js
+++ scripts/units.js
@@ -1,8 +1,12 @@
 {
-	//region - everything
+	//// TANGERINE ////
 	const wtt = extend(UnitType, "wtt", {});
 	wtt.constructor = () => extend(MechUnit, {});
 	
+	//// ROLEPLAY ////
+	// BLAST //
 	const mechBc = extend(UnitType, "mech-bc", {});
+	mechBc.constructor = () => extend(MechUnit, {});
+	const bulkBc = extend(UnitType, "bulk-bc", {});
 	mechBc.constructor = () => extend(MechUnit, {});
 }
