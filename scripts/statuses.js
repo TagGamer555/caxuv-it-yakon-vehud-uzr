cd65f7a11726fe0d43f3041a8ae8389f37a9e2e2
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-11-10 21:44:27 +0000

    aaa

--- scripts/statuses.js
+++ scripts/statuses.js
@@ -4,3 +4,12 @@ const reloadtier3 = (StatusEffect, "reload-tier3", {});
 const healthtier1 = (StatusEffect, "health-tier1", {});
 const healthtier2 = (StatusEffect, "health-tier2", {});
 const healthtier3 = (StatusEffect, "health-tier3", {});
+
+module.exports = {
+	reloadtier1: reloadtier1,
+	reloadtier2: reloadtier2,
+	reloadtier3: reloadtier3,
+	healthtier1: healthtier1,
+	healthtier2: healthtier2,
+	healthtier3: healthtier3
+}
