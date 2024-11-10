bf56bef2380b3284b3c15777b28791ab17b34a92
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-11-10 21:20:03 +0000

    awwwwwww

--- scripts/statuses.js
+++ scripts/statuses.js
@@ -1,6 +1,6 @@
-const reloadtier1 = (, "reload-tier1", {});
-const reloadtier2 = (, "reload-tier2", {});
-const reloadtier3 = (, "reload-tier3", {});
-const healthtier1 = (, "health-tier1", {});
-const healthtier2 = (, "health-tier2", {});
-const healthtier3 = (, "health-tier3", {});
+const reloadtier1 = (StatusEffect, "reload-tier1", {});
+const reloadtier2 = (StatusEffect, "reload-tier2", {});
+const reloadtier3 = (StatusEffect, "reload-tier3", {});
+const healthtier1 = (StatusEffect, "health-tier1", {});
+const healthtier2 = (StatusEffect, "health-tier2", {});
+const healthtier3 = (StatusEffect, "health-tier3", {});
