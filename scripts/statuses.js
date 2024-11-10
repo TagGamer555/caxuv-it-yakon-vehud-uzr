4b602a0f4ebaded7cab5d0396390f88079b29be0
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-11-10 21:18:10 +0000

    wiw

--- scripts/statuses.js
+++ scripts/statuses.js
@@ -1,3 +1,6 @@
 const reloadtier1 = (, "reload-tier1", {});
 const reloadtier2 = (, "reload-tier2", {});
 const reloadtier3 = (, "reload-tier3", {});
+const healthtier1 = (, "health-tier1", {});
+const healthtier2 = (, "health-tier2", {});
+const healthtier3 = (, "health-tier3", {});
